2178453532cc8817b6b7c031f1ba633d906f3028
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-07-02 16:02:45 +0530

    update

---
+++ slider/src/App.js
@@ -0,0 +1,22 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+import CarouselComponent from './CarouselComponent ';
+
+
+
+function App() {
+  return (
+    <div className="App">
+   
+        <h1>React Bootstrap Carousel</h1>
+        <CarouselComponent />
+    
+   
+  
+    </div>
+  );
+}
+
+export default App;
